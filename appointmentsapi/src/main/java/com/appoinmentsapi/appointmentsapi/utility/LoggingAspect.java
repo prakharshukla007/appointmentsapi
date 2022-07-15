fed7fdcf9b4e4932ae60e7ced2947025f11dbbf3
Wesker- <Wesker-@LAPTOP-3CNN2O3V>
2022-07-15 14:38:08 +0530

    Some final fixes

--- appointmentsapi/src/main/java/com/appoinmentsapi/appointmentsapi/utility/LoggingAspect.java
+++ appointmentsapi/src/main/java/com/appoinmentsapi/appointmentsapi/utility/LoggingAspect.java
@@ -4,6 +4,8 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.aspectj.lang.annotation.AfterThrowing;
 import org.aspectj.lang.annotation.Aspect;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -12,9 +14,16 @@ public class LoggingAspect {
 	
 	public static final Log LOGGER = LogFactory.getLog(LoggingAspect.class);
 	
-	@AfterThrowing(pointcut = "execution(* com.appointmentsapi.appointmentsapi.service.*Impl.*(..))", throwing = "exception")
+	@Autowired
+	Environment env;
+	
+	@AfterThrowing(pointcut = "execution(* com.appoinmentsapi.appointmentsapi.service.*Impl.*(..))", throwing = "exception")
 	public void logServiceException(Exception exception) {
-		LOGGER.error(exception.getMessage(), exception);
+		//Below code can be un-commented to print the stack trace of the exception
+		//LOGGER.error(env.getProperty(exception.getMessage()), exception);
+		
+		//With this, only the configured message is displayed
+		LOGGER.error(env.getProperty(exception.getMessage()));
 	}
 	
 }
